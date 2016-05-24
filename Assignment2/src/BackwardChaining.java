8ce15cc010f26bdbcd20e6a9738777699a7de23a
daledesilva <daledesilva@gmail.com>
2016-05-24 18:12:45 +1000

    Backward Chaining Done
    Backward Chaining is done and is more robust and readable than Forward
    Chaining. See Test_Backward chaining for how it handles decoy sentences
    that end in dead ends.
    
    Forward Chaining will be cleaned up to be similar

--- Assignment2/src/BackwardChaining.java
+++ Assignment2/src/BackwardChaining.java
@@ -1,19 +1,145 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class BackwardChaining extends Method {
 	
-	public BackwardChaining(ArrayList<Sentence> aKB, String aQ)
+	Literal fQuery;
+	Stack<Sentence> fChainSteps;
+	
+	public BackwardChaining(ArrayList<Sentence> aKB, Literal aQuery)
 	{
 		super(aKB);		// save the knowledgebase using the superclasses constructor
+		fQuery = aQuery;
 	}
 	
+	
+	
+	// Finds steps in the chain that can be used to solve the query
+	// Saves those steps in fChainSteps
+	// returns true if the query can be solved or false if not 
 	@Override
-	public boolean solve()
+	public boolean isSolvable()
 	{
-		boolean result = false;
+		// fReadyToSolve is initialised to false in parent class;
 		
-		// solve and save string result into fResult (defined in superclass)
 		
-		return result;
+		// check everything in aKB is a horn sentence
+		for( Sentence lSentence : fKB )
+		{
+			if(!lSentence.isHornClause())
+			{
+				// TODO: throw an error here
+				//"Not all sentences are Horn Clauses - Cannot use Backward Chaining";
+				return false;	// abort the process
+			}
+		}
+		
+		
+		// sentences that haven't had their Literals explored yet
+		ArrayList<Sentence> lUnknownSentences = fKB;
+		
+		// steps found leading back from fQuery
+		fChainSteps = new Stack<>();
+		
+		// The list of literals we've found so far
+		Stack<Literal> lKnownLiterals = new Stack<>();
+				
+		// The list of literals we need to find solutions for
+		Queue<Literal> lNeededLiterals = new LinkedList<>();
+		lNeededLiterals.add(fQuery);
+		
+		
+		
+		Literal lNeededLiteral;
+		
+		// keep looping while there are literals needed
+		NeededLiteralLoop:
+		while( !lNeededLiterals.isEmpty() )
+		{
+			lNeededLiteral = lNeededLiterals.remove();	// get the first needed literal
+			
+			// loop all sentences left with unknown positiveLiterals
+			for( Sentence lSentence : lUnknownSentences )
+			{
+				
+				// if the needed literal is on the right hand side of the implication
+				if( lNeededLiteral == lSentence.getPositiveLiteral() )
+				{
+					// we've found a sentence that will give us the needed literal
+					
+					// remove sentence from unknownSentences and add to chainSteps
+					lUnknownSentences.remove(lSentence);
+					fChainSteps.push(lSentence);
+					
+					// put the literal into knownLiterals stack
+					lKnownLiterals.push( lNeededLiteral );
+				
+					// if it's not a fact (a 1 literal sentence)
+					if(lSentence.sentenceLength() > 1 )
+					{
+						// add all literals on the left to the needed list
+						for(int k = 0; k<lSentence.otherLiteralsLength(); k++)
+							if( !lKnownLiterals.contains(lSentence.getOtherLiteral(k)) )	// not if they're already known
+								if( !lNeededLiterals.contains(lSentence.getOtherLiteral(k)) )	// not if they're already in the needed list
+									lNeededLiterals.add(lSentence.getOtherLiteral(k));
+					}
+					
+					// if there's no more literals needed
+					if(lNeededLiterals.isEmpty())
+					{
+						// then no need to search anymore
+						fReadyToSolve = true;
+						break NeededLiteralLoop;
+					} else {
+						// otherwise, skip to the next literal needed
+						continue NeededLiteralLoop;
+					}
+				}
+				
+			} // SentenceLoop end
+			
+			
+			// if this is reached the literal wasn't found as implied in any sentence
+			// erase the lNeededLiterals stack as it contains literals that contribute to a sentence that can't be solved
+			lNeededLiterals.clear();
+			// add the last found literal back into lNeededLiterals to look for a different route
+			// (the sentence that was used has already been removed so it won't see that again)
+			lNeededLiterals.add( lKnownLiterals.pop() );
+			// remove the last added item in fChainSteps also as it's not longer a valid step
+			fChainSteps.pop();
+			
+			// TODO: should remove the irrelevant items off the chain too
+			
+			
+		} // NeededLiteralLoop end
+		
+		
+		return fReadyToSolve;
 	}
+	
+	
+	@Override
+	public String printSolutionList()
+	{
+		String lMessage = "";
+		
+		if(!fChainSteps.isEmpty())
+		{
+			for(int k=fChainSteps.size()-1; k>=0; k-- )
+			{
+				Sentence lSentence = fChainSteps.get(k);
+				if(k!=fChainSteps.size()-1)
+					lMessage += ", ";
+				lMessage +=	lSentence.getPositiveLiteral().getName();
+			}
+		}
+		
+		return lMessage;
+	}
+	
+	
+	
+	
+	
+	
 }
---
+++ Assignment2/src/Test_BackwardChaining.java
@@ -0,0 +1,116 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class Test_BackwardChaining {
+
+	
+	
+	//
+	//	TESTS TO CHECK FOR HORN SENTENCES PROPERLY
+	//
+	
+	@Test
+	public void BackwardChaining_TestNonHornSentence_Simple() throws Exception {
+				
+		ArrayList<Sentence> lKB = new ArrayList<>();
+		lKB.add( new Sentence("p2=> p3&g") );
+
+		Literal lQueryLiteral = lKB.get(0).getLiteral( lKB.get(0).literalsLength()-1 );
+		assertEquals( lQueryLiteral.getName(), "g" );	// fail safe to check we're actually trying to solve for the correct literal
+		
+		BackwardChaining lMethod = new BackwardChaining(lKB, lQueryLiteral);
+		
+		assertFalse( lMethod.isSolvable() );	// check it doesn't solve it
+		
+	}
+	
+	
+	@Test
+	public void BackwardChaining_TestNonHornSentence_Complex() throws Exception {
+				
+		ArrayList<Sentence> lKB = new ArrayList<>();
+		lKB.add( new Sentence("p2&w\\/~p5=> p3&g\\/h<=>y") );
+		
+		Literal lQueryLiteral = lKB.get(0).getLiteral( lKB.get(0).literalsLength()-1 );
+		assertEquals( lQueryLiteral.getName(), "y" );	// fail safe to check we're actually trying to solve for the correct literal
+		
+		BackwardChaining lMethod = new BackwardChaining(lKB, lQueryLiteral);
+		
+		assertFalse( lMethod.isSolvable() );	// check it doesn't solve it
+		
+	}
+	
+	
+	@Test
+	public void BackwardChaining_TestNonHornSentence_MultipleImplications() throws Exception {
+				
+		ArrayList<Sentence> lKB = new ArrayList<>();
+		lKB.add( new Sentence("p2=>p3&g=>y") );
+
+		Literal lQueryLiteral = lKB.get(0).getLiteral( lKB.get(0).literalsLength()-1 );
+		assertEquals( lQueryLiteral.getName(), "y" );	// fail safe to check we're actually trying to solve for the correct literal
+		
+		BackwardChaining lMethod = new BackwardChaining(lKB, lQueryLiteral);
+		
+		assertFalse( lMethod.isSolvable() );	// check it doesn't solve it
+		
+	}
+	
+	
+	
+	//
+	// TESTS TO CHECK ON STANDARD SOLVES
+	//
+	
+	@Test
+	public void BackwardChaining_TestStraightForwardRoute() throws Exception {
+				
+		// separated onto multiple lines for clarity
+		String lKB = "";
+		lKB += "p2=> p3;";		// 3
+		lKB += "p3 => p1;";		// 2
+		lKB += "c => e;";
+		lKB += "b&e => f;";
+		lKB += "f&g => h;";
+		lKB += "p1=>d;";		// 1st
+		lKB += "p1&p3 => c;";
+		lKB += "a;";
+		lKB += "b;";
+		lKB += "p2;";			// 4
+		
+		InferenceEngine iEngine = new InferenceEngine(lKB);
+		
+		assertTrue( iEngine.prepareSolution("d", "BC") );	// returns true if a route is found
+		
+	}
+	
+	
+	@Test
+	public void BackwardChaining_TestDeceptiveRoute() throws Exception {
+		
+		// separated onto multiple lines for clarity
+		String lKB = "";
+		lKB += "p2=> p3;";				// 5
+		lKB += "decoy2 => decoy1;";		// 3
+		lKB += "decoy1 => p1;";			// 2	(as this is earlier, it will be checked before the line below)
+		lKB += "p3 => p1;";				// 4
+		lKB += "c => e;";
+		lKB += "b&e => f;";
+		lKB += "f&g => h;";
+		lKB += "p1=>d;";				// 1st
+		lKB += "p1&p3 => c;";
+		lKB += "a;";
+		lKB += "b;";
+		lKB += "p2;";					// 6
+		
+		InferenceEngine iEngine = new InferenceEngine(lKB);
+		
+		assertTrue( iEngine.prepareSolution("d", "BC") );	// returns true if a route is found
+	}
+	
+	
+
+}
